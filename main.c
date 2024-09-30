b236eff7178af840787bd8845f901bceeac7385b
Daniel Nicholson <danielnicholson04@outlook.com>
2024-09-30 11:09:22 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // TODO: build a linked-list version of this array
+    int numbers[] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}
+
